1bb91230c10d4893d303534d0a74d378654261d6
KienNT <kiennt@rabiloo.com.vn>
2021-03-27 00:11:05 +0700

    modify

--- android/src/main/java/com/example/wifiip/WifiIpPlugin.java
+++ android/src/main/java/com/example/wifiip/WifiIpPlugin.java
@@ -48,13 +48,58 @@ public class WifiIpPlugin implements MethodCallHandler {
 
       result = new HashMap<>();
       result.put("ip", intToIp(dhcpInfo.ipAddress));
-      result.put("netmask", intToIp(dhcpInfo.netmask));
+//      result.put("netmask", intToIp(dhcpInfo.netmask));
       result.put("broadcastIP", intToIp((dhcpInfo.ipAddress & dhcpInfo.netmask) | ~dhcpInfo.netmask));
+
+      int bit = 24;
+      try {
+        List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+        for (NetworkInterface nif : all) {
+          if (nif.getName().equalsIgnoreCase("wlan0")) {
+            try {
+              bit = nif.getInterfaceAddresses().get(1).getNetworkPrefixLength();
+            } catch (Exception ignored) {
+            }
+          } else if (nif.getName().equalsIgnoreCase("eth0")) {
+            try {
+              bit = nif.getInterfaceAddresses().get(1).getNetworkPrefixLength();
+            } catch (Exception ignored) {
+            }
+          }
+        }
+      } catch (Exception ex) {
+        bit = 24;
+      }
+
+      result.put("netmask", getSubnetMark(bit));
       return result;
     } catch (Exception e) {
       Log.e(TAG, e.getMessage());
     }
     return null;
+  }
+
+  String getSubnetMark(int bit) {
+    StringBuilder subnetString = new StringBuilder();
+    for (int i = 1; i<=32; i++) {
+      if (i <= bit) {
+        subnetString.append("1");
+      } else {
+        subnetString.append("0");
+      }
+      if (i % 8 == 0 && i != 32) {
+        subnetString.append(".");
+      }
+    }
+    String[] subnets = subnetString.toString().split("\\.");
+    StringBuilder subnet = new StringBuilder();
+    for (int i = 0; i< subnets.length; i++ ){
+      subnet.append(Integer.parseInt(subnets[i], 2));
+      if(i< subnets.length - 1){
+        subnet.append(".");
+      }
+    }
+    return subnet.toString();
   }
 
   String intToIp(int i) {
